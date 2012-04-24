fe88f8e095348c077993699a42262a8d94f4cc63
Alexander Orlov <alexander.orlov@loxal.net>
2012-04-24 11:22:04 +0200

    NEXT iteration

--- src/db/data.dart
+++ src/db/data.dart
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2012 Alexander Orlov <alexander.orlov@loxal.net>. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+#import('loxal:testData');
+
 class Data {
 
   Data() {
