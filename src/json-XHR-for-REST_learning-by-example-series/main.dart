352ef692a9278d51d7c298d2e04a794da6255a01
Alexander Orlov <alexander.orlov@loxal.net>
2012-05-18 18:52:45 +0200

    +LBE

--- src/json-XHR-for-REST_learning-by-example-series/main.dart
+++ src/json-XHR-for-REST_learning-by-example-series/main.dart
@@ -1,6 +1,20 @@
 #library('loxal:LBE');
 
-hello1() => print('world2');
-main(){
-    hello1();
+#import('dart:html');
+#import('/Users/alex/my/src/DartJSONP/DartJSONP.dart');
+
+void showTweets() {
+  var callbackMethod = "callbackMethod";
+  final JsonpCallback twitterCallback = new JsonpCallback(callbackMethod);
+  
+  twitterCallback.onDataReceived = (Map data) {
+    print(data);
+  };
+
+  var twitterUrl = "http://search.twitter.com/search.json?q=dartlangg&callback=$callbackMethod";
+  twitterCallback.doCallback(twitterUrl);
+}
+
+void main() { // here is where the app execution starts
+  showTweets();  
 }
