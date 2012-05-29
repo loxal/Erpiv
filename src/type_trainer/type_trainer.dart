fe1755c8c637a341b7ca506cd0f35eaf85da230e
Alexander Orlov <alexander.orlov@loxal.net>
2012-05-30 00:41:36 +0200

    + typetrainer functionality skeleton

--- src/type_trainer/type_trainer.dart
+++ src/type_trainer/type_trainer.dart
@@ -1,6 +1,7 @@
 #library('loxal:typetrainer');
 
 #import('dart:html');
+#import('dart:math');
 
 class TypeTrainer {
     MarqueeElement marquee;
@@ -14,17 +15,24 @@ TypeTrainer(String scrollingText) {
     """);
     marqueeInner.behavior = 'alternate';
   marquee = new Element.html("""
-      <marquee direction="left" width="250" height="200" style="border:solid;">
-      $scrollingText
-    </marquee>
+      <marquee direction="left" height="20" style="border: solid;">$scrollingText</marquee>
     """);
-    marquee.behavior = 'alternate';
+    marquee.behavior = 'scroll';
 
     document.body.elements.add(marquee);
-    print(marquee.behavior);
+//    marquee.scrollAmount = 3;
     print(marquee.scrollAmount);
+//    marquee.scrollDelay = 300;
+    marquee.trueSpeed = false;
+    marquee.loop = 2;
+    marquee.vspace = 100;
+    marquee.hspace = 100;
+    marquee.width = '50%';
     print(marquee.scrollDelay);
     print(marquee.trueSpeed);
+    print(marquee.loop);
+    print(marquee.hspace);
+    print(marquee.vspace);
 //    marquee.elements.add(marqueeInner);
 //    marquee.stop();
 //    marqueeInner.stop();
@@ -34,23 +42,35 @@ TypeTrainer(String scrollingText) {
 
 
 void bindHandlers() {
-                   document.on.keyPress.add((KeyboardEvent event) {
+                   window.on.keyPress.add((KeyboardEvent event) {
                         String keyLiteral = new String.fromCharCodes([event.charCode]);
-                      print(keyLiteral);
-                      typeTrainer.marquee.innerHTML = keyLiteral;
+                        typeTrainer.marquee.stop();
+                        if(keyLiteral == typeTrainer.marquee.text[0]) { // replace by startsWith
+                            print(keyLiteral);
+                            typeTrainer.marquee.text = typeTrainer.marquee.text.substring(1);
+                                               typeTrainer.marquee.scrollDelay = 400;
+                        }
+                        typeTrainer.marquee.start();
+//                        typeTrainer.marquee.innerHTML = keyLiteral;
                     });
 }
 
-List<String> generateText() {
+String generateText() {
+    String text = '';
+    Random random = new Random();
+    for(var i = 1; i < 13; i++) {
+        int letterCode = random.nextInt(26) + 97;
+        text += new String.fromCharCodes([letterCode]);
+        if(i%3 === 0) {
+            text += ' ';
+        }
+    }
 
-    return "Bouncable text.";
+    return text;
 }
 
 TypeTrainer typeTrainer;
 main() {
     bindHandlers();
     typeTrainer = new TypeTrainer(generateText());
-    typeTrainer.marqueeInner.innerHTML = 'boom!';
-
-
 }
