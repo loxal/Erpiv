9a3ebcde8076a57ee5cc42c7a25ea6c935f8139c
Alexander Orlov <alexander.orlov@loxal.net>
2012-05-29 21:22:22 +0200

    type trainer discoveries

--- src/type_trainer/type_trainer.dart
+++ src/type_trainer/type_trainer.dart
@@ -3,20 +3,54 @@
 #import('dart:html');
 
 class TypeTrainer {
-TypeTrainer() {
-  var test = new Element.html("""
-  <marquee direction="down" width="250" height="200" behavior="alternate" style="border:solid">
-  <marquee behavior="alternate">
-    This text will bounce
-  </marquee>
-</marquee>
-""");
+    MarqueeElement marquee;
+    MarqueeElement marqueeInner;
 
-    document.body.elements.add(test);
+TypeTrainer(String scrollingText) {
+    marqueeInner = new Element.html("""
+      <marquee>
+        $scrollingText
+    </marquee>
+    """);
+    marqueeInner.behavior = 'alternate';
+  marquee = new Element.html("""
+      <marquee direction="left" width="250" height="200" style="border:solid;">
+      $scrollingText
+    </marquee>
+    """);
+    marquee.behavior = 'alternate';
+
+    document.body.elements.add(marquee);
+    print(marquee.behavior);
+    print(marquee.scrollAmount);
+    print(marquee.scrollDelay);
+    print(marquee.trueSpeed);
+//    marquee.elements.add(marqueeInner);
+//    marquee.stop();
+//    marqueeInner.stop();
 }
 
 }
 
+
+void bindHandlers() {
+                   document.on.keyPress.add((KeyboardEvent event) {
+                        String keyLiteral = new String.fromCharCodes([event.charCode]);
+                      print(keyLiteral);
+                      typeTrainer.marquee.innerHTML = keyLiteral;
+                    });
+}
+
+List<String> generateText() {
+
+    return "Bouncable text.";
+}
+
+TypeTrainer typeTrainer;
 main() {
-    new TypeTrainer();
+    bindHandlers();
+    typeTrainer = new TypeTrainer(generateText());
+    typeTrainer.marqueeInner.innerHTML = 'boom!';
+
+
 }
