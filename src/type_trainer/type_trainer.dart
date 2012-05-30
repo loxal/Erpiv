854715f204cabf537ca60e4a3657d3d1db9af203
Alexander Orlov <alexander.orlov@loxal.net>
2012-05-30 11:07:40 +0200

    basic type trainer works

--- src/type_trainer/type_trainer.dart
+++ src/type_trainer/type_trainer.dart
@@ -5,72 +5,64 @@
 
 class TypeTrainer {
     MarqueeElement marquee;
-    MarqueeElement marqueeInner;
 
-TypeTrainer(String scrollingText) {
-    marqueeInner = new Element.html("""
-      <marquee>
-        $scrollingText
-    </marquee>
+    static final int spaceCharCode = 32;
+    static final String cursor = '|';
+    final String spaceChar;
+
+TypeTrainer(String scrollingText) : spaceChar = new String.fromCharCodes([spaceCharCode]) {
+    bindHandlers();
+    scrollingText = generateText();
+
+    marquee = new Element.html("""
+      <marquee style="font-size: 4em; border: solid; color: yellow; height: 1.3em;">$scrollingText</marquee>
     """);
-    marqueeInner.behavior = 'alternate';
-  marquee = new Element.html("""
-      <marquee direction="left" height="20" style="border: solid;">$scrollingText</marquee>
-    """);
-    marquee.behavior = 'scroll';
+    marquee.behavior = 'slide';
 
     document.body.elements.add(marquee);
-//    marquee.scrollAmount = 3;
+    marquee.scrollAmount = 5;
     print(marquee.scrollAmount);
-//    marquee.scrollDelay = 300;
-    marquee.trueSpeed = false;
-    marquee.loop = 2;
-    marquee.vspace = 100;
-    marquee.hspace = 100;
-    marquee.width = '50%';
+    marquee.scrollDelay = 10;
+//    marquee.trueSpeed = false;
+//    marquee.loop = 2;
+    marquee.bgColor = 'blue';
+    marquee.vspace = 0;
+    marquee.hspace = 0;
+    marquee.width = '100%';
     print(marquee.scrollDelay);
     print(marquee.trueSpeed);
     print(marquee.loop);
-    print(marquee.hspace);
-    print(marquee.vspace);
-//    marquee.elements.add(marqueeInner);
-//    marquee.stop();
-//    marqueeInner.stop();
 }
 
-}
+    void bindHandlers() {
+        window.on.keyPress.add((final KeyboardEvent event) {
+            final String keyLiteral = new String.fromCharCodes([event.charCode]);
+            validateLetter(keyLiteral);
+        });
+    }
 
-
-void bindHandlers() {
-                   window.on.keyPress.add((KeyboardEvent event) {
-                        String keyLiteral = new String.fromCharCodes([event.charCode]);
-                        typeTrainer.marquee.stop();
-                        if(keyLiteral == typeTrainer.marquee.text[0]) { // replace by startsWith
-                            print(keyLiteral);
-                            typeTrainer.marquee.text = typeTrainer.marquee.text.substring(1);
-                                               typeTrainer.marquee.scrollDelay = 400;
-                        }
-                        typeTrainer.marquee.start();
-//                        typeTrainer.marquee.innerHTML = keyLiteral;
-                    });
-}
-
-String generateText() {
-    String text = '';
-    Random random = new Random();
-    for(var i = 1; i < 13; i++) {
-        int letterCode = random.nextInt(26) + 97;
-        text += new String.fromCharCodes([letterCode]);
-        if(i%3 === 0) {
-            text += ' ';
+    void validateLetter(final String keyLiteral) {
+        if(typeTrainer.marquee.text[1] == keyLiteral) {
+            typeTrainer.marquee.text = '|' + typeTrainer.marquee.text.substring(2);
         }
     }
 
-    return text;
+    String generateText([int numOfLetters = 13, int spaceCharAfter = 3]) {
+        String text = cursor;
+        final Random random = new Random();
+        for(var i = 1; i < numOfLetters; i++) {
+            int letterCode = random.nextInt(26) + 97;
+            text += new String.fromCharCodes([letterCode]);
+            if(i % spaceCharAfter === 0) {
+                text += spaceChar;
+            }
+        }
+
+        return text;
+    }
 }
 
 TypeTrainer typeTrainer;
 main() {
-    bindHandlers();
-    typeTrainer = new TypeTrainer(generateText());
+    typeTrainer = new TypeTrainer('');
 }
