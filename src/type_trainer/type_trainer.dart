17996cd2e2f19ffdfc820fc286b0f8263f8bbaf0
Alexander Orlov <alexander.orlov@loxal.net>
2012-05-30 14:12:32 +0200

    mistake rate calculation

--- src/type_trainer/type_trainer.dart
+++ src/type_trainer/type_trainer.dart
@@ -9,56 +9,111 @@ class TypeTrainer {
     static final int spaceCharCode = 32;
     static final String cursor = '|';
     final String spaceChar;
+    Function keyPressHandler;
+    static final comparableIdx = 1;
+    int totalChars;
+    int mistakeCount = 0;
 
-TypeTrainer(String scrollingText) : spaceChar = new String.fromCharCodes([spaceCharCode]) {
-    bindHandlers();
-    scrollingText = generateText();
+    void buildControls() {
+    //    https://upload.wikimedia.org/wikipedia/commons/0/04/Keyboard_layout_ru%28typewriter%29.svg
+    //Dvorak: http://upload.wikimedia.org/wikipedia/en/a/a6/KB_Programmer_Dvorak.svg
+    //English / Hebrew: https://upload.wikimedia.org/wikipedia/commons/e/e6/Touch_typing-he.svg
+    // English: http://upload.wikimedia.org/wikipedia/commons/2/29/Touch_typing.svg
+    // German: https://upload.wikimedia.org/wikipedia/commons/6/60/QWERTZ-10Finger-Layout_W.svg
+        Element tenFingerLayout = new Element.html("""
+        <img src="http://upload.wikimedia.org/wikipedia/commons/2/29/Touch_typing.svg" width=100%/>
+        """);
+        document.body.elements.add(tenFingerLayout);
 
-    marquee = new Element.html("""
-      <marquee style="font-size: 4em; border: solid; color: yellow; height: 1.3em;">$scrollingText</marquee>
-    """);
-    marquee.behavior = 'slide';
-
-    document.body.elements.add(marquee);
-    marquee.scrollAmount = 5;
-    print(marquee.scrollAmount);
-    marquee.scrollDelay = 10;
-//    marquee.trueSpeed = false;
-//    marquee.loop = 2;
-    marquee.bgColor = 'blue';
-    marquee.vspace = 0;
-    marquee.hspace = 0;
-    marquee.width = '100%';
-    print(marquee.scrollDelay);
-    print(marquee.trueSpeed);
-    print(marquee.loop);
-}
-
-    void bindHandlers() {
-        window.on.keyPress.add((final KeyboardEvent event) {
-            final String keyLiteral = new String.fromCharCodes([event.charCode]);
-            validateLetter(keyLiteral);
+        ButtonElement restart = new Element.html("""
+            <button>Restart</button>
+        """);
+        restart.on.click.add((ClickEvent event) { marquee.text = generateText();
+        marquee.stop();
+        marquee.start();
+        marquee.blur();
+        marquee.contentEditable = 'true';
         });
+        document.body.elements.add(restart);
     }
 
-    void validateLetter(final String keyLiteral) {
-        if(typeTrainer.marquee.text[1] == keyLiteral) {
-            typeTrainer.marquee.text = '|' + typeTrainer.marquee.text.substring(2);
+    TypeTrainer(String scrollingText) : spaceChar = new String.fromCharCodes([spaceCharCode]) {
+        bindHandlers();
+        scrollingText = generateText();
+
+        marquee = new Element.html("""
+          <marquee style="font-size: 4em; border: solid; color: yellow; height: 1.3em;">$scrollingText</marquee>
+        """);
+        marquee.behavior = 'slide';
+
+        document.body.elements.add(marquee);
+        marquee.scrollAmount = 5;
+        marquee.scrollDelay = 10;
+        marquee.loop = 1;
+        marquee.bgColor = '#119';
+        marquee.width = '100%';
+
+        buildControls();
+    }
+
+    void bindHandlers() {
+        keyPressHandler = (final KeyboardEvent event) {
+            final String char = new String.fromCharCodes([event.charCode]);
+            validateChar(char);
+        };
+
+        window.on.keyPress.add(keyPressHandler);
+    }
+
+    void unbindHandlers() {
+        window.on.keyPress.remove(keyPressHandler);
+    }
+
+    void validateChar(final String keyLiteral) {
+        if(typeTrainer.marquee.text[comparableIdx] == keyLiteral) {
+            typeTrainer.marquee.bgColor = '#119';
+            typeTrainer.marquee.text = cursor + typeTrainer.marquee.text.substring(comparableIdx + 1);
+            if(hasFinished()) finished();
+        } else {
+            showMistake();
         }
     }
 
-    String generateText([int numOfLetters = 13, int spaceCharAfter = 3]) {
+    void showMistake() {
+        mistakeCount++;
+        typeTrainer.marquee.bgColor = '#911';
+    }
+
+    String generateText([int totalChars = 5, int spaceCharAfter = 3]) {
+        this.totalChars = totalChars;
         String text = cursor;
         final Random random = new Random();
-        for(var i = 1; i < numOfLetters; i++) {
+        for(var i = 1; i < totalChars; i++) {
             int letterCode = random.nextInt(26) + 97;
             text += new String.fromCharCodes([letterCode]);
             if(i % spaceCharAfter === 0) {
                 text += spaceChar;
             }
         }
-
         return text;
+    }
+
+    boolean hasFinished() {
+      return typeTrainer.marquee.text.length === 1;
+    }
+
+    void finished() {
+        unbindHandlers();
+                marquee.scrollAmount = 100;
+                marquee.scrollDelay = 0;
+        typeTrainer.marquee.text = 'Finished!';
+
+        calculateStats();
+    }
+
+    void calculateStats() {
+        final float mistakeRate = mistakeCount / totalChars;
+        print('mistakeRate: ${mistakeRate * 100}%');
     }
 }
 
