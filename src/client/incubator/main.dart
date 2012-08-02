2ba27afb47a33733a7a23fcbfd6ce36c82249910
Alexander Orlov <alexander.orlov@loxal.net>
2012-08-02 08:48:27 +0200

    + incubator concept

--- src/client/incubator/main.dart
+++ src/client/incubator/main.dart
@@ -1,45 +1,22 @@
 #library('loxal:Incubator');
 
 #import('dart:html');
+#import('dart:uri');
 
 void main() {
 
-  Dynamic d =
+  final String uriSearch = window.location.search;
+  final List<String> paramValuePairs = uriSearch.substring(1).split('&');
+  final paramMaps = new List<String>();
+  paramValuePairs.forEach((paramValuePair) => paramMaps.addAll(paramValuePair.split('=')));
 
-  void myFunc() {
-    print('void');
-  };
+  var paramMapping = new HashMap<String, String>();
+//  print(paramMaps[2]);
+  paramMapping.putIfAbsent();
 
-//  d();
-
-  var d1 =
-
-  void myFunc() {
-    print('another void');
-  };
-
-//  d1();
-
-  d1;
-  d;
-
-  int lineCount;
-//  assert(lineCount == null); // Variables (even numbers) are initially null.
-//  assert(lineCount != null); // Variables (even numbers) are initially null.
-
-  var sqr =
-
-      (x) => x * x;
-  print(sqr(5));
-
-  print((
-
-          (x) => x * x)(10));
-
-  ImageElement image = new ImageElement();
-  image.src = 'http://upload.wikimedia.org/wikipedia/commons/c/c4/Blackeyegalaxy.jpg';
-  image.on.load.add((event) {
-    window.console.log(image.height); // Will always print 0.
-  });
+//print(paramMaps);
+//print(paramMaps[1]);
+//  paramMapping.putIfAbsent('',() => 'eee');
+//  print(paramMapping['module']);
 
 }
