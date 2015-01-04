ffaf6f1ebd18fd4958d4cd7b2addfc8e95d4ad3e
Alex <alexander.orlov@loxal.net>
2015-01-04 22:18:39 +0100

    port to the current version of Dart SDK

--- src/websockets/server.dart
+++ src/websockets/server.dart
@@ -1,4 +1,10 @@
-#import('dart:io');
+/*
+ * Copyright 2015 Alexander Orlov <alexander.orlov@loxal.net>. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+import 'dart:io';
 
 void main() {
   HttpServer server = new HttpServer();
